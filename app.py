9a0866d853bc359ca561a0ea9ec2d2219a665daa
akshay-niranjan <akshayniranjan51@gmail.com>
2025-03-29 14:38:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,request
-# import requests
+import requests
 
 app = Flask(__name__)
 
@@ -10,20 +10,20 @@ def index():
     amount=data['queryResult']['parameters']['unit-currency']['amount']
     target_currency=data['queryResult']['parameters']['currency-name']
 
-    print(source_money)
+    print(source_currency)
     print(amount)
-    print(target_money)
-    # cf = fetch_conversion_factor(source_currency,target_currency)  # using conversion api
-    # print("cf",cf)
+    print(target_currency)
+    cf = fetch_conversion_factor(source_currency,target_currency)  # using conversion api
+    print("cf",cf)
     return "Hello ! This is a currency_conversion_bot backend page"
 
-# def fetch_conversion_factor(source,target):
-#     url="https://currencyconversionapi.com/api/v1/live?access_key=46ddee37c04ebb446376e21613d3bfff"
-#     response=requests.get(url)
-#     data=response.json()
-#     quotes=data.get("quotes",{})
-#     conversion_key=f"{source}{target}"
-#     rate=quotes[conversion_key]
-#     return rate
+def fetch_conversion_factor(source,target):
+    url="https://currencyconversionapi.com/api/v1/live?access_key=46ddee37c04ebb446376e21613d3bfff"
+    response=requests.get(url)
+    data=response.json()
+    quotes=data.get("quotes",{})
+    conversion_key=f"{source}{target}"
+    rate=quotes[conversion_key]
+    return rate
 if __name__ == "__main__":
     app.run(debug=True)
