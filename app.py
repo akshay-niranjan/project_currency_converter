f8e6ece4c3e9496e94cec2fc7e65d51d18e31069
akshay-niranjan <akshayniranjan51@gmail.com>
2025-03-29 14:53:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 import requests
 
 app = Flask(__name__)
@@ -10,12 +10,17 @@ def index():
     amount=data['queryResult']['parameters']['unit-currency']['amount']
     target_currency=data['queryResult']['parameters']['currency-name']
 
-    print(source_currency)
-    print(amount)
-    print(target_currency)
+    # print(source_currency)
+    # print(amount)
+    # print(target_currency)
     cf = fetch_conversion_factor(source_currency,target_currency)  # using conversion api
-    print("cf",cf)
-    return "Hello ! This is a currency_conversion_bot backend page"
+    # print("cf",cf)
+    final_amount=amount*cf
+    resp={
+        'fulfillmentText':"{} {} is {} {}".format(amount,source_currency,final_amount,target_currency)
+    }
+    print(final_amount)
+    return jsonify(response)
 
 def fetch_conversion_factor(source,target):
     url="https://currencyconversionapi.com/api/v1/live?access_key=46ddee37c04ebb446376e21613d3bfff"
